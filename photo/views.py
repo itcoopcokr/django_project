6e9c74c61574f31e4a8f8b4685b698ce5891f9d9
lux600 <thinkmentor@gmail.com>
2017-12-22 16:48:00 +0900

    form

--- photo/views.py
+++ photo/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 from django.views.generic import (
     ListView,
     DetailView,
+    CreateView,
     UpdateView,
     DeleteView,
 )
 
 from .models import Photo
+from .forms import PhotoForm
 # Create your views here.
 
 class PhotoListView(ListView):
@@ -17,5 +19,10 @@ class PhotoListView(ListView):
 class PhotoDetailView(DetailView):
     model = Photo
     template_name = 'photo/photo_detail.html'
+
+class PhotoCreateView(CreateView):
+    model = Photo
+    template_name = 'photo/photo_create.html'
+    form_class = PhotoForm
 
 

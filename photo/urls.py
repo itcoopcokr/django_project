4fb5305d7da9f5e03b394124d3ba20db1604b90d
lux600 <thinkmentor@gmail.com>
2017-12-26 09:52:22 +0900

    photo create

--- photo/urls.py
+++ photo/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls.static import url, static
 from .views import (
     PhotoListView,
     PhotoDetailView,
+    PhotoCreateView,
 )
 
 urlpatterns = [
     url(r'^$', PhotoListView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$',PhotoDetailView.as_view(), name='detail'),
+    url(r'^create/$',PhotoCreateView.as_view(), name='create'),
 
 ]
